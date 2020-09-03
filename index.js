33f32ed85eeffe214f394ea12471c4e47310e524
Amber Kerawala Torres <amber_kerawala@berkeley.edu>
2020-09-03 05:52:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 // Add your functions here
 
-function map
+function map(source, callback) {
+  let result = []
+  for (let i = 0; i < source.length; i++) {
+    result.push(callback(source[i]))
+  }
+  return result;
+}
